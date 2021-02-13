f0f7802e5a9950e99bece0f4abb7bb9e7a7fa07f
alejandro.perez1 <alejandro.perez1@udea.edu.co>
2021-02-13 17:30:52 -0500

    [ADD] jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,9 @@ const compression = require('compression');
 require('express-async-errors'); //Ayuda a capturar en un middleware las excepciones
 
 //requeruimos los middleware
-const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares');
+const { NotFoundMiddleware, ErrorMiddleware, AuthMiddleware } = require('../middlewares');
 
-module.exports = function({HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes}){
+module.exports = function({HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, AuthRoutes}){
     const router = express.Router();
     const apiRoutes = express.Router();
 
@@ -23,11 +23,13 @@ module.exports = function({HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes}){
     apiRoutes.use("/user", UserRoutes);
     apiRoutes.use("/idea", IdeaRoutes);
     apiRoutes.use("/comment", CommentRoutes);
+    apiRoutes.use("/auth", AuthRoutes);
 
     router.use("/v1/api", apiRoutes);
 
     router.use(NotFoundMiddleware);
     router.use(ErrorMiddleware);
+    router.use(AuthMiddleware);
 
     return router;    
 };
